c715f13466e78b217719f6a7f60d1936e61fae33
Zofia Nora <sophienora92@gmail.com>
2025-03-29 11:51:54 +0100

    created the basektball component, made the basketball draggedable

--- src/components/Basketball.jsx
+++ src/components/Basketball.jsx
@@ -1,9 +1,44 @@
 import imageBasketball from '../assets/basketball.png';
+import { useState } from "react";
+
 
 function Basketball() {
+    const [position, setPosition] = useState({ x: 50, y: 50 });
+    const [dragging, setDragging] = useState(false);
+
+    const handleMouseDown = (e) => {
+        setDragging(true);
+    };
+
+    const handleMouseMove = (e) => {
+        if (!dragging) return;
+        setPosition({ x: e.clientX - 25, y: e.clientY - 25 });
+    };
+
+    const handleMouseUp = () => {
+        setDragging(false);
+    };
+
+
     return (
         <>
-            <img src={imageBasketball} alt="" />
+           <img
+                src={imageBasketball}
+                alt="Basketball"
+                className="basketball"
+                style={{
+                    position: "absolute",
+                    left: `${position.x}px`,
+                    top: `${position.y}px`,
+                    width: "50px",
+                    height: "50px",
+                    cursor: "grab",
+                    zIndex: 10,
+                }}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={handleMouseUp}
+            />
         </>
     )
 }
