b3ad2394ec9b1ba20b7d732f243f6bf55f949aa1
Zofia Nora <sophienora92@gmail.com>
2025-03-30 12:30:47 +0200

    added card but static

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <>
+            <div id="container">
+                <div id="card"></div>
+            </div>
+        </>
+    )
+}
+
+export default Card;
