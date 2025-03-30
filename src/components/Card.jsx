20142439f17d3bff4f8caf5aca5c9af819a24612
Zofia Nora <sophienora92@gmail.com>
2025-03-30 12:39:48 +0200

    changed colors

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,48 @@ function Card() {
     return (
         <>
             <div id="container">
-                <div id="card"></div>
+                <div 
+                    id="card">
+                    <div id="header">
+                        <h1>Drag me!</h1>
+                    </div>
+                </div>
             </div>
         </>
     )
-}
+};
 
 export default Card;
+
+
+
+/* let newX = 0, newY = 0, startX = 0, startY = 0;
+
+const card = document.getElementById('card')
+
+card.addEventListener('mousedown', mouseDown)
+
+function mouseDown(e){
+    startX = e.clientX
+    startY = e.clientY
+
+    document.addEventListener('mousemove', mouseMove)
+    document.addEventListener('mouseup', mouseUp)
+}
+
+function mouseMove(e){
+    newX = startX - e.clientX 
+    newY = startY - e.clientY 
+  
+    startX = e.clientX
+    startY = e.clientY
+
+    card.style.top = (card.offsetTop - newY) + 'px'
+    card.style.left = (card.offsetLeft - newX) + 'px'
+}
+
+function mouseUp(e){
+    document.removeEventListener('mousemove', mouseMove)
+}
+
+*/
